da4841e6078a111a2b368a38b30576ba68e36b2b
BienBeo <tranngocbien2809@gmail.com>
2024-11-28 15:35:53 +0700

    Applying PropTypes

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,14 +1,16 @@
+import PropTypes from 'prop-types';
 import { forwardRef, useState } from 'react';
 import images from '~/assets/images';
 import styles from './Image.module.scss';
 import classNames from 'classnames';
 
-const Image = forwardRef(({ src, className, fallback: customFallback = images.noImage,...props }, ref) => {
+const Image = forwardRef(({ src, className, fallback: customFallback = images.noImage, ...props }, ref) => {
     const [fallback, setFallback] = useState('');
     const handleError = () => {
         setFallback(customFallback);
     };
     return (
+        // eslint-disable-next-line jsx-a11y/alt-text
         <img
             className={classNames(styles.wrapper, className)}
             ref={ref}
@@ -18,5 +20,11 @@ const Image = forwardRef(({ src, className, fallback: customFallback = images.no
         />
     );
 });
+
+Image.propTypes = {
+    src: PropTypes.string,
+    className: PropTypes.string,
+    fallback: PropTypes.string,
+};
 
 export default Image;
